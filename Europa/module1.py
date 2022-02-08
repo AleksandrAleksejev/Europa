e84fd8f03b3efb0bd86ca7457bd94d823ca092d0
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2022-02-08 22:55:43 +0300

    т

--- Europa/module1.py
+++ Europa/module1.py
@@ -7,29 +7,8 @@ for line in file:
     Riigids[k.strip()] = v.strip()
 
    
-def main():
-  global Riigids
-  print("Добро пожаловать в Карту Европейских стран и столиц!")
-  print()
-  print(" 1 - Найти страну/столицу  \n 2 - Добавить новую страну \n 3 - Исправить ошибку  \n 4 - Проверка знаний.")
-  valik=int(input("-> "))
-  if valik not in [1,2,3,4]:
-     print("ВЫбери от 1 до 4!")
-  else:
-    if valik==1:
-      naiti(Riigids)
-    if valik==2:
-      dobavit()
-    if valik==3:
-        oshibka()
-    if valik==4:
-        test()
 
-def naiti(r:dict):
-  global Riigids
-  print("Напиши Страну/Город")
-  slovo=input("->")
-  print(Riigids.get(slovo), "<-")
+
 
 
 def dobavit():
@@ -48,25 +27,7 @@ def dobavit():
  print("Сохранено!")
 
 
-def oshibka():
-    b=""
-    c=""
-    print("Страна:")
-    a=str(input("-> "))
-    print("Город:")
-    b=str(input("-> "))
-    if a not in Riigids and b not in Riigids:
-        print("Такой страны или города нект в списке!")
-    else:
-        print("Страна:")
-        strana=str(input("-> "))
-        print("Город:")
-        stolitsal=str(input("-> "))
-        b += str(strana) + "-" + str(stolitsa)
-        c += str(stolitsa) + "-" + str(strana)
-        file=open("riigid.txt","a")
-        file.write(b+"\n")
-        file.write(c+"\n")
+
 
 def test():
     p = 0
@@ -94,3 +55,30 @@ def test():
             print("У тебя",pr,"Процентов")
         else:
             print("Ну ладно :((")
+
+def naiti(r:dict):
+  global Riigids
+  print("Напиши Страну/Город")
+  slovo=input("->")
+  print(Riigids.get(slovo), "<-")
+
+
+def oshibka():
+    b=""
+    c=""
+    print("Страна:")
+    a=str(input("-> "))
+    print("Город:")
+    b=str(input("-> "))
+    if a not in Riigids and b not in Riigids:
+        print("Такой страны или города нект в списке!")
+    else:
+        print("Страна:")
+        strana=str(input("-> "))
+        print("Город:")
+        stolitsal=str(input("-> "))
+        b += str(strana) + "-" + str(stolitsa)
+        c += str(stolitsa) + "-" + str(strana)
+        file=open("riigid.txt","a")
+        file.write(b+"\n")
+        file.write(c+"\n")
